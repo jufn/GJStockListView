2decbee57a778c0260ca62f7b2deb66ac1df9e13
陈俊峰 <junfeng.chen1@yintech.cn>
2021-04-12 17:20:15 +0800

    [feat] 创建新的滚动控制器

---
+++ Example/GJStockListView/StockList/MTNStockListViewController_new.h
@@ -0,0 +1,17 @@
+//
+//  MTNStockListViewController_new.h
+//  GJStockListView_Example
+//
+//  Created by 陈俊峰 on 2021/4/12.
+//  Copyright © 2021 jufn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MTNStockListViewController_new : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/GJStockListView/StockList/MTNStockListViewController_new.m
@@ -0,0 +1,32 @@
+//
+//  MTNStockListViewController_new.m
+//  GJStockListView_Example
+//
+//  Created by 陈俊峰 on 2021/4/12.
+//  Copyright © 2021 jufn. All rights reserved.
+//
+
+#import "MTNStockListViewController_new.h"
+
+@interface MTNStockListViewController_new ()
+
+@end
+
+@implementation MTNStockListViewController_new
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
