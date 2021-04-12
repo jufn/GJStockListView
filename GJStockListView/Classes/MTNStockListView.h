cb8f439cc7ac65213b673c8c5e78ec4a54034ac7
陈俊峰 <junfeng.chen1@yintech.cn>
2021-04-12 17:26:26 +0800

    [feat] 添加文件

---
+++ GJStockListView/Classes/MTNStockListView.h
@@ -0,0 +1,31 @@
+//
+//  MTNStockListView.h
+//  GJStockListView
+//
+//  Created by 陈俊峰 on 2021/4/12.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+@class MTNStockListView;
+@protocol MTNStockListViewDelegate <UITableViewDelegate>
+- (CGFloat)stockListView:(MTNStockListView *)stockListView widthForItem:(NSInteger)item section:(NSInteger)section;
+- (void)stockListView:(MTNStockListView *)stockListView itemViewAtItem:(NSInteger)item row:(NSInteger)row section:(NSInteger)section;
+@end
+
+@protocol MTNStockListViewDataSource  <UITableViewDataSource>
+
+- (BOOL)stockListView:(MTNStockListView *)stockListView shouldHorizontalScrollableAtSection:(NSInteger)section;
+- (NSInteger)stockListView:(MTNStockListView *)stockListView numberOfItemsAtSection:(NSInteger)section;
+
+@end
+
+@interface MTNStockListView : UITableView
+
+@property (nonatomic, weak, nullable) id<MTNStockListViewDelegate>delegate;
+@property (nonatomic, weak, nullable) id<MTNStockListViewDataSource>dataSource;
+
+@end
+
+NS_ASSUME_NONNULL_END
