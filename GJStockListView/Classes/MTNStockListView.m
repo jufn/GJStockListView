c8eeb6b9edf6f12a9d683f48d945b1f5ecebc123
陈俊峰 <junfeng.chen1@yintech.cn>
2021-04-19 16:12:13 +0800

    [feat] 完善forward逻辑

--- GJStockListView/Classes/MTNStockListView.m
+++ GJStockListView/Classes/MTNStockListView.m
@@ -28,17 +28,153 @@ NSString *getSectionIdentifier(NSInteger section) {
 }
 @end
 
-@interface MTNStockListViewForwardTarget : NSObject <UITableViewDelegate, UITableViewDataSource>
+@interface MTNStockListViewForwardTarget : NSObject <UITableViewDelegate, UITableViewDataSource, MTNScrollableRowViewDelegate>
+@property (nonatomic, strong) NSMutableDictionary <NSString *, MTNSectionConfigure *> *sectionConfigure;
 
 @property (nonatomic, weak) id <MTNStockListViewDelegate>delegate;
 @property (nonatomic, weak) id <UITableViewDataSource>dataSource;
+@property (nonatomic, weak) MTNStockListView *listView;
 @end
 
-@interface MTNStockListView () <MTNScrollableRowViewDelegate>
+@implementation MTNStockListViewForwardTarget
 
-@property (nonatomic, strong) NSMutableDictionary <NSString *, MTNSectionConfigure *> *sectionConfigure;
+- (BOOL)respondsToSelector:(SEL)aSelector {
+    return [super respondsToSelector:aSelector] || [self.delegate respondsToSelector:aSelector];
+}
+
+- (void)forwardInvocation:(NSInvocation *)anInvocation {
+    [anInvocation invokeWithTarget:self.delegate];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    BOOL should = NO;
+    if ([self.delegate respondsToSelector:@selector(tableView:cellForRowAtIndexPath:)]) {
+        should = [self.delegate stockListView:(MTNStockListView *)tableView shouldHorizontalScrollableAtSection:indexPath.section];
+    }
+    
+    if (should == NO) {
+        return [self.dataSource tableView:tableView cellForRowAtIndexPath:indexPath];
+    }
+    
+    static NSInteger kScrollableRowViewTag = 10086l;
+    NSString *identifier = getSectionIdentifier(indexPath.section);
+    MTNSectionConfigure *configure = [self sectionConfigureAtSection:indexPath.section];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+        CGFloat rowHeight = [self.delegate tableView:tableView heightForRowAtIndexPath:indexPath];
+        
+        MTNScrollableRowView *view = [[MTNScrollableRowView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(tableView.frame), rowHeight) numberOfItems:[self numberOfItemsAtSection:indexPath.section] delegate:self];
+        view.tag = kScrollableRowViewTag;
+        [cell.contentView addSubview:view];
+        [configure.rowViews addPointer:(__bridge void *)view];
+    }
+    MTNScrollableRowView *rowView = [cell.contentView viewWithTag:kScrollableRowViewTag];
+    [rowView setContentOffsetX:configure.contentOffsetX];
+    rowView.indexPath = indexPath;
+    return cell;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.dataSource tableView:tableView numberOfRowsInSection:section];
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    NSInteger num = 1;
+    if ([self.dataSource respondsToSelector:@selector(numberOfSectionsInTableView:)]) {
+        num = [self.dataSource numberOfSectionsInTableView:tableView];
+    }
+    return num;
+}
+
+#pragma mark - MTNScrollableTableViewCellDelegate
+
+- (nonnull NSAttributedString *)rowView:(nonnull MTNScrollableRowView *)view attributedStringForItem:(NSInteger)item {
+    NSAttributedString *attri = nil;
+    if (view.isAddedToHeader) { // 头部
+        attri = [self attrbutedStringForHeaderItem:item section:view.indexPath.section];
+    } else {
+        attri = [self attributedStringForItem:item row:view.indexPath.row section:view.indexPath.section];
+    }
+    return attri;
+}
+
+- (CGFloat)rowView:(nonnull MTNScrollableRowView *)view widthForItem:(NSInteger)item {
+    NSIndexPath *indexPath = view.indexPath;
+    CGFloat width = 0.0f;
+    if ([self.delegate respondsToSelector:@selector(stockListView:widthForItem:section:)]) {
+        width = [self.delegate stockListView:self.listView widthForItem:item section:indexPath.section];
+    }
+    return width;
+}
+
+- (void)rowView:(MTNScrollableRowView *)view didScrollToOffsetX:(CGFloat)x {
+    MTNSectionConfigure *configure = [self sectionConfigureAtSection:view.indexPath.section];
+    
+    configure.contentOffsetX  = x;
+    [configure.rowViews compact];
+    for (MTNScrollableRowView *each in configure.rowViews) {
+        if ([each isEqual:view] == NO) {
+            [each setContentOffsetX:x];
+        }
+    }
+}
+
+- (void)rowView:(MTNScrollableRowView *)view didSelectedAtItem:(NSInteger)item {
+    [self.listView selectRowAtIndexPath:view.indexPath animated:YES scrollPosition:UITableViewScrollPositionNone];
+    if (self.delegate && [self.delegate respondsToSelector:@selector(tableView:didSelectRowAtIndexPath:)]) {
+        [self.delegate tableView:self.listView didSelectRowAtIndexPath:view.indexPath];
+    }
+}
+
+#pragma mark - <UITableViewDelegate, UITableViewDataSource>
+
+- (MTNSectionConfigure *)sectionConfigureAtSection:(NSInteger)section {
+    NSString *identifier = getSectionIdentifier(section);
+    MTNSectionConfigure *configure = self.sectionConfigure[identifier];
+    if (configure == nil) {
+        configure = [MTNSectionConfigure new];
+        [self.sectionConfigure setObject:configure forKey:identifier];
+    }
+    return configure;
+}
+
+- (NSInteger)numberOfItemsAtSection:(NSInteger)section {
+    NSInteger number = 0;
+    if ([self.delegate respondsToSelector:@selector(stockListView:numberOfItemsAtSection:)]) {
+        number = [self.delegate stockListView:self.listView numberOfItemsAtSection:section];
+    }
+    return number;
+}
+
+- (NSAttributedString *)attrbutedStringForHeaderItem:(NSInteger)item section:(NSInteger)section {
+    NSAttributedString *attri = nil;
+    if (self.delegate && [self.delegate respondsToSelector:@selector(stockListView:attributedStringForHeaderItem:section:)]) {
+        attri = [self.delegate stockListView:self.listView attributedStringForHeaderItem:item section:section];
+    }
+    return attri;
+}
+
+- (NSAttributedString *)attributedStringForItem:(NSInteger)item row:(NSInteger)row section:(NSInteger)section {
+    NSAttributedString *attri = nil;
+    if (self.delegate && [self.delegate respondsToSelector:@selector(stockListView:attributedStringForItem:row:section:)]) {
+        attri = [self.delegate stockListView:self.listView attributedStringForItem:item row:row section:section];
+    }
+    return attri;
+}
+
+#pragma mark - Getter
+- (NSMutableDictionary<NSString *,MTNSectionConfigure *> *)sectionConfigure {
+    if (!_sectionConfigure) {
+        _sectionConfigure = [NSMutableDictionary dictionary];
+    }
+    return _sectionConfigure;
+}
+
+@end
+
+@interface MTNStockListView ()
 @property (nonatomic, strong) MTNStockListViewForwardTarget *forwardTarget;
-
 @end
 
 @implementation MTNStockListView
@@ -67,154 +203,12 @@ NSString *getSectionIdentifier(NSInteger section) {
     return self.forwardTarget.dataSource;
 }
 
-#pragma mark - MTNScrollableTableViewCellDelegate
-
-- (nonnull NSAttributedString *)rowView:(nonnull MTNScrollableRowView *)view attributedStringForItem:(NSInteger)item {
-    NSAttributedString *attri = nil;
-    if (view.isAddedToHeader) { // 头部
-        attri = [self attrbutedStringForHeaderItem:item section:view.indexPath.section];
-    } else {
-        attri = [self attributedStringForItem:item row:view.indexPath.row section:view.indexPath.section];
-    }
-    return attri;
-}
-
-- (CGFloat)rowView:(nonnull MTNScrollableRowView *)view widthForItem:(NSInteger)item {
-    NSIndexPath *indexPath = view.indexPath;
-    CGFloat width = 0.0f;
-    if ([self.delegate respondsToSelector:@selector(stockListView:widthForItem:section:)]) {
-        width = [self.delegate stockListView:self widthForItem:item section:indexPath.section];
-    }
-    return width;
-}
-
-- (void)rowView:(MTNScrollableRowView *)view didScrollToOffsetX:(CGFloat)x {
-    MTNSectionConfigure *configure = [self sectionConfigureAtSection:view.indexPath.section];
-    
-    configure.contentOffsetX  = x;
-    [configure.rowViews compact];
-    for (MTNScrollableRowView *each in configure.rowViews) {
-        if ([each isEqual:view] == NO) {
-            [each setContentOffsetX:x];
-        }
-    }
-}
-
-- (void)rowView:(MTNScrollableRowView *)view didSelectedAtItem:(NSInteger)item {
-    [self selectRowAtIndexPath:view.indexPath animated:YES scrollPosition:UITableViewScrollPositionNone];
-    if (self.delegate && [self.delegate respondsToSelector:@selector(tableView:didSelectRowAtIndexPath:)]) {
-        [self.delegate tableView:self didSelectRowAtIndexPath:view.indexPath];
-    }
-}
-
-#pragma mark - <UITableViewDelegate, UITableViewDataSource>
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    BOOL should = NO;
-    if ([self.delegate respondsToSelector:@selector(tableView:cellForRowAtIndexPath:)]) {
-        should = [self.delegate stockListView:self shouldHorizontalScrollableAtSection:indexPath.section];
-    }
-    
-    if (should == NO) {
-        return [self.dataSource tableView:self cellForRowAtIndexPath:indexPath];
-    }
-    
-    static NSInteger kScrollableRowViewTag = 10086l;
-    NSString *identifier = getSectionIdentifier(indexPath.section);
-    MTNSectionConfigure *configure = [self sectionConfigureAtSection:indexPath.section];
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
-    if (cell == nil) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
-        CGFloat rowHeight = [self.delegate tableView:self heightForRowAtIndexPath:indexPath];
-        
-        MTNScrollableRowView *view = [[MTNScrollableRowView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(tableView.frame), rowHeight) numberOfItems:[self numberOfItemsAtSection:indexPath.section] delegate:self];
-        view.tag = kScrollableRowViewTag;
-        [cell.contentView addSubview:view];
-        [configure.rowViews addPointer:(__bridge void *)view];
-    }
-    MTNScrollableRowView *rowView = [cell.contentView viewWithTag:kScrollableRowViewTag];
-    [rowView setContentOffsetX:configure.contentOffsetX];
-    rowView.indexPath = indexPath;
-    return cell;
-}
-
-- (MTNSectionConfigure *)sectionConfigureAtSection:(NSInteger)section {
-    NSString *identifier = getSectionIdentifier(section);
-    MTNSectionConfigure *configure = self.sectionConfigure[identifier];
-    if (configure == nil) {
-        configure = [MTNSectionConfigure new];
-        [self.sectionConfigure setObject:configure forKey:identifier];
-    }
-    return configure;
-}
-
-- (NSInteger)numberOfItemsAtSection:(NSInteger)section {
-    NSInteger number = 0;
-    if ([self.delegate respondsToSelector:@selector(stockListView:numberOfItemsAtSection:)]) {
-        number = [self.delegate stockListView:self numberOfItemsAtSection:section];
-    }
-    return number;
-}
-
-- (NSAttributedString *)attrbutedStringForHeaderItem:(NSInteger)item section:(NSInteger)section {
-    NSAttributedString *attri = nil;
-    if (self.delegate && [self.delegate respondsToSelector:@selector(stockListView:attributedStringForHeaderItem:section:)]) {
-        attri = [self.delegate stockListView:self attributedStringForHeaderItem:item section:section];
-    }
-    return attri;
-}
-
-- (NSAttributedString *)attributedStringForItem:(NSInteger)item row:(NSInteger)row section:(NSInteger)section {
-    NSAttributedString *attri = nil;
-    if (self.delegate && [self.delegate respondsToSelector:@selector(stockListView:attributedStringForItem:row:section:)]) {
-        attri = [self.delegate stockListView:self attributedStringForItem:item row:row section:section];
-    }
-    return attri;
-}
-
-#pragma mark - Getter
-- (NSMutableDictionary<NSString *,MTNSectionConfigure *> *)sectionConfigure {
-    if (!_sectionConfigure) {
-        _sectionConfigure = [NSMutableDictionary dictionary];
-    }
-    return _sectionConfigure;
-}
-
 - (MTNStockListViewForwardTarget *)forwardTarget {
     if (!_forwardTarget) {
         _forwardTarget = [MTNStockListViewForwardTarget new];
+        _forwardTarget.listView = self;
     }
     return _forwardTarget;
 }
 
 @end
-
-@implementation MTNStockListViewForwardTarget
-
-- (BOOL)respondsToSelector:(SEL)aSelector {
-    return [super respondsToSelector:aSelector] || [self.delegate respondsToSelector:aSelector];
-}
-
-- (void)forwardInvocation:(NSInvocation *)anInvocation {
-    [anInvocation invokeWithTarget:self.delegate];
-}
-
-- (UITableViewCell *)tableView:(MTNStockListView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
-    return [tableView tableView:tableView cellForRowAtIndexPath:indexPath];
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return [self.dataSource tableView:tableView numberOfRowsInSection:section];
-}
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    NSInteger num = 1;
-    if ([self.dataSource respondsToSelector:@selector(numberOfSectionsInTableView:)]) {
-        num = [self.dataSource numberOfSectionsInTableView:tableView];
-    }
-    return num;
-}
-
-@end
-
-
